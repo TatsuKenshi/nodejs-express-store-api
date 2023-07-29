2efd759ababfadc7609f778b61c98f15b2875671
TatsuKenshi <rockinbg@gmail.com>
2023-07-29 18:29:46 +0200

    mongoDB database connection established

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
-const notFound = require("./middleware/not-found");
 
 // middleware
 app.use(express.json());
@@ -25,6 +26,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connect db
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening to the port ${port}...`));
   } catch (error) {
     console.log(error);
